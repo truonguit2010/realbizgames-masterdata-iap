7b7e327af8799d515bac56f733ac850f351e2fab
DarkNight <truonguit2010@gmail.com>
2021-06-13 09:38:30 +0700

    - Support Data Validation.

---
+++ Runtime/IAP/Validation/IAPServiceValidationPiece.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using ChainPattern;
+
+public class IAPServiceValidationPiece : IAsynPieceExecutor
+{
+    const string TAG = "IAPServiceValidationPiece";
+
+    private IIAPService iAPService;
+
+    public bool IsDone => _result != null;
+
+    public IAsynPieceResult Result => _result;
+
+    private IAPServiceValidationPieceResult _result;
+
+    public IAPServiceValidationPiece(IIAPService iAPService)
+    {
+        this.iAPService = iAPService;
+    }
+
+    public void Execute(IAsynChainResult data)
+    {
+        #if UNITY_EDITOR
+        UnityEngine.Debug.LogFormat("{0} - Execute", TAG);
+        #endif
+        List<IAPDTO> dtos = iAPService.GetAllIAPItems();
+        #if UNITY_EDITOR
+        string msg = ToStringForList(dtos);
+        UnityEngine.Debug.LogFormat("{0} - {1}", TAG, msg);
+        #endif
+        
+        _result = new IAPServiceValidationPieceResult(success: true);
+
+        #if UNITY_EDITOR
+        UnityEngine.Debug.LogFormat("{0} - End", TAG);
+        #endif
+    }
+
+    public static string ToStringForList<T>(List<T> l) {
+        string message = "Total: " + l.Count + "\n";
+        foreach (T o in l) {
+            message += o.ToString() + "\n";
+        }
+        return message;
+    }
+}
---
+++ Runtime/IAP/Validation/IAPServiceValidationPieceResult.cs
@@ -0,0 +1,21 @@
+﻿using ChainPattern;
+
+public class IAPServiceValidationPieceResult : IAsynPieceResult
+{
+    public const string PUBLIC_KEY = "IAPServiceValidationPieceResult";
+
+    private bool _success;
+
+    public IAPServiceValidationPieceResult(bool success)
+    {
+        _success = success;
+    }
+
+    public bool Success { get => _success; set => _success = value; }
+
+    public string GetKey()
+    {
+        return PUBLIC_KEY;
+    }
+}
+
